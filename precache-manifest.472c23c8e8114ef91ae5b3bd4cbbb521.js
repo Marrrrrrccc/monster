f35164f59b7fbc195180f29b681f3ba3c25bc026
Marc Ricson Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2021-06-07 10:42:37 +0800

    Updates

---
+++ precache-manifest.472c23c8e8114ef91ae5b3bd4cbbb521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5008a21da91ce3a0100d24a7b63d23",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "d18c8fd4c8052c09e7c3",
+    "url": "/monster/static/css/main.ac03ac47.chunk.css"
+  },
+  {
+    "revision": "78f96b92dc0c6c461866",
+    "url": "/monster/static/js/2.ad8eb8ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster/static/js/2.ad8eb8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18c8fd4c8052c09e7c3",
+    "url": "/monster/static/js/main.bea6030e.chunk.js"
+  },
+  {
+    "revision": "562e33ad7bf5e068a5dc",
+    "url": "/monster/static/js/runtime-main.745f0dd4.js"
+  }
+]);
